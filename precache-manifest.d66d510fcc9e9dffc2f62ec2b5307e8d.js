5e41315362e66b3b852abcace38022cb77261729
izzyandrade <israelcarneiro97@gmail.com>
2022-04-19 23:41:58 -0300

    Updates

---
+++ precache-manifest.d66d510fcc9e9dffc2f62ec2b5307e8d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d595fa661087adf0d7d4c82c3d662d1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8d4e996c6db190fcab1b",
+    "url": "/portfolio/static/css/main.24b05912.chunk.css"
+  },
+  {
+    "revision": "35272af9b46e3190755e",
+    "url": "/portfolio/static/js/2.0c264335.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/portfolio/static/js/2.0c264335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d4e996c6db190fcab1b",
+    "url": "/portfolio/static/js/main.18b9c290.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "054c14e0d1a44f2924b141255f8d0cda",
+    "url": "/portfolio/static/media/homepage.054c14e0.png"
+  },
+  {
+    "revision": "c9c7ce1e131284ce3d8fe85900186259",
+    "url": "/portfolio/static/media/me.c9c7ce1e.png"
+  },
+  {
+    "revision": "fb73051ab9df8e28b2df1b9e44b682d3",
+    "url": "/portfolio/static/media/project.fb73051a.png"
+  },
+  {
+    "revision": "e5f72452a7e8defdfad2e1b621601104",
+    "url": "/portfolio/static/media/wave.e5f72452.svg"
+  }
+]);
